676040fbd0fbbc0f330609ca19317eb5648f0b67
Debajyoti Maity <75745343+e-hakson@users.noreply.github.com>
2023-09-05 08:47:57 +0530

    Create Print-adjacency-list.java

---
+++ Problem of the Day/Print-adjacency-list.java
@@ -0,0 +1,17 @@
+//User function Template for Java
+class Solution {
+    public List<List<Integer>> printGraph(int V, int edges[][]) {
+        List<List<Integer>> al = new  ArrayList<>();
+              for( int i =0 ; i<V;i++)
+              {
+                  al.add(new ArrayList<Integer>());
+              }
+            for( int i =0 ; i<edges.length;i++)
+            {
+               al.get(edges[i][0]).add(edges[i][1]);
+               al.get(edges[i][1]).add(edges[i][0]);
+            }
+            // al=adj;
+            return al;
+    }
+}
