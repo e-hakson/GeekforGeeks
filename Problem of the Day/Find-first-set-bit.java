ce42b9ceafb5bb4ab7641f236d6bac1ec82f42b3
Debajyoti Maity <75745343+e-hakson@users.noreply.github.com>
2023-09-19 15:19:55 +0530

    Easy

---
+++ Problem of the Day/Find-first-set-bit.java
@@ -0,0 +1,18 @@
+//User function Template for Java
+
+class Solution
+{
+    //Function to find position of first set bit in the given number.
+    public static int getFirstSetBit(int n){
+            
+        // Your code here
+        if(n==0) return 0;
+        int i = 1;
+        while(n>0 && n%2==0){
+            n = n/2;
+            i++;
+        }
+        return i;
+        
+    }
+}
